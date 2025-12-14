035470043e3b34d7a064a68d6b6a492682b396ba
krish-ezhu <krishezhu2005@gmail.com>
2025-12-14 15:39:44 +0530

    Add files via upload

---
+++ Q19.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter a number  : ";
+    cin>>num;
+    cout<<" Absalute of given Number  : "<<abs(num);
+    return 0;
+}
