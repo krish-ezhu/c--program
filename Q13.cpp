0cb54ab095a42e007c4236897046294b77f489c0
krish-ezhu <krishezhu2005@gmail.com>
2025-12-14 14:56:27 +0530

    Add files via upload

---
+++ Q13.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    int num1,num2,mod;
+    cout<<"Enter a number 1 : ";
+    cin>>num1;
+    cout<<"Enter a number 2 : ";
+    cin>>num2;
+    mod=num1%num2;
+    cout<<mod;
+    return 0;
+}
