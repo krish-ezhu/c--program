309dd3af9cd08a74ebe60259b10722f44f8156a9
krish-ezhu <krishezhu2005@gmail.com>
2025-12-14 17:32:46 +0530

    Update Q14.cpp

--- Q14.cpp
+++ Q14.cpp
@@ -1,14 +1,15 @@
+//Write a C++ Program to print quotient of two numbers.
 #include <iostream>
 #include <iomanip>
 using namespace std;
 int main()
 {
-    int num1,num2,quatient;
+    int num1,num2,quotient;
     cout<<"Enter a number 1 : ";
     cin>>num1;
     cout<<"Enter a number 2 : ";
     cin>>num2;
-    quatient=num1/num2;
-    cout<<quatient;
+    quotient=num1/num2;
+    cout<<quotient;
     return 0;
 }
