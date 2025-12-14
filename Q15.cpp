b9071ac89cd9d36f2c40a873425862572cd3125b
krish-ezhu <krishezhu2005@gmail.com>
2025-12-14 15:08:25 +0530

    Add files via upload

---
+++ Q15.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    int num1,num2,temp;
+    cout<<"Enter a number 1 : ";
+    cin>>num1;
+    cout<<"Enter a number 2 : ";
+    cin>>num2;
+    temp=num1;
+    num1=num2;
+    num2=temp;
+    cout<<" Number 1 : "<<num1<<endl<<" Number 2 : "<<num2;
+    return 0;
+}
