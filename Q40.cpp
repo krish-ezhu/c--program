cedd136ee62105199ee0dfc7cc5ca2bbed189066
Krishnakumar Elumalai <krishezhu2005@gmail.com>
2025-12-16 17:56:57 +0530

    Create Q40.cpp

---
+++ Q40.cpp
@@ -0,0 +1,19 @@
+// Write a C++  program to print all factors of a given number.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "Factors of " << num << " are: ";
+
+    for (int i = 1; i <= num; i++) {
+        if (num % i == 0)
+            cout << i << " ";
+    }
+
+    return 0;
+}
