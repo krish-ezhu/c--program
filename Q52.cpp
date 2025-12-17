78a43acdc88217ca8c0bbdf5d313176d94125f2b
Krishnakumar Elumalai <krishezhu2005@gmail.com>
2025-12-17 18:08:42 +0530

    Create Q52.cpp

---
+++ Q52.cpp
@@ -0,0 +1,10 @@
+// Write a C++ program to print all English alphabets in lowercase.
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 97; i <= 122; i++) {
+        cout << char(i) << " ";
+    }
+    return 0;
+}
